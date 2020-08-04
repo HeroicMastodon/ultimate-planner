cc9070490bbc0d11b744b9dcb8e667812a161104
Garrick <garrick.hilliard@gmail.com>
2020-08-03 23:27:29 -0600

    Created the skeleton of taks

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <TaskList></TaskList>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import TaskList from "@/components/TaskList/TaskList.vue";
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    TaskList
   }
 });
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
